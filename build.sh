0d2862e7c008d22fd060adfcc1c32f0748394512
qiuye2015 <1119345739@qq.com>
2021-01-22 11:38:44 +0800

    httpserver&prometheus

---
+++ build.sh
@@ -0,0 +1,8 @@
+#########################################################################
+# File Name: build.sh
+# Author: fjp
+# mail: fjp@xxx.com
+# Created Time: Tue Jan 19 10:23:25 2021
+#########################################################################
+#!/bin/bash
+go build  -o stock_monitor src/prometheus/main.go
