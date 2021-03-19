f18e3e0f3e03e17a5f4bebe7edf50b035f4e9542
qiuye2015 <1119345739@qq.com>
2021-03-19 15:13:30 +0800

    bk GoFrameWork

--- build.sh
+++ build.sh
@@ -2,7 +2,15 @@
 # File Name: build.sh
 # Author: fjp
 # mail: fjp@xxx.com
-# Created Time: Tue Jan 19 10:23:25 2021
+# Created Time: Thu Apr 30 11:01:38 2020
 #########################################################################
 #!/bin/bash
-go build  -o stock_monitor src/prometheus/main.go
+
+src_file=$1
+if [ ! $src_file ];then
+	echo Usage: `basename $0` src_directory
+	exit
+fi
+go build -gcflags=all="-N -l" $1
+## 必须这样编译，才能用gdb打印出变量，第二个是小写的L，不是大写的i
+#go build  -o stock_monitor src/prometheus/main.go
