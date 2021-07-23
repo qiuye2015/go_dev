8fb10ded8b3dd7195262f6519bd71eb0b3e09cf2
qiuye2015 <1119345739@qq.com>
2021-07-23 20:00:41 +0800

    test

--- src/test.go
+++ src/test.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 )
 
 func init() {
@@ -13,12 +10,12 @@ func init() {
 }
 
 func main() {
-	resp, err := http.Get("http://httpbin.org/get?name=luozhiyun&age=27")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
+	//resp, err := http.Get("http://httpbin.org/get?name=luozhiyun&age=27")
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//defer resp.Body.Close()
+	//body, _ := ioutil.ReadAll(resp.Body)
+	//fmt.Println(string(body))
 }
